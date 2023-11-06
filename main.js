8645519354b459999a91c922f1b4f689de8bf7ca
ClemZ08 <clement.rfd@gmail.com>
2023-11-06 13:43:57 +0100

    uptade2

--- main.js
+++ main.js
@@ -1,9 +1,9 @@
 const helloWorld = React.createElement('h1', {}, 'Hello world!');
-
+const test = <h1>Bonjour!</h1>;
 
 // mauvaise pratique
 
 
 //ReactDOM.render(helloWorld, document.body);
 
-ReactDOM.render(helloWorld, document.querySelector('#app'));
+ReactDOM.render(test, document.querySelector('#app'));
