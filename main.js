5f0f71b52f9658e4d4585968516a7b9c6a63bcda
ClemZ08 <clement.rfd@gmail.com>
2023-11-07 11:34:27 +0100

    uptade

--- main.js
+++ main.js
@@ -34,4 +34,39 @@ function LastName(props) {
 
 const helloWorld = <h1>Hello <FirstName text={firstName}/> <LastName text={lastName}/></h1>;
 
-ReactDOM.render(helloWorld, document.querySelector('#app'));
+
+class Clock extends React.Component {
+    constructor(props) {
+      super(props);
+      this.state = {date: new Date()};
+    }
+  
+    componentDidMount() {
+      this.timerID = setInterval(
+        () => this.tick(),
+        1000
+      );
+    }
+  
+    componentWillUnmount() {
+      clearInterval(this.timerID);
+    }
+  
+    tick() {
+      this.setState({
+        date: new Date()
+      });
+    }
+  
+    render() {
+      return (
+        <div>
+          <h1>Bonjour, monde !</h1>
+          <h2>Il est {this.state.date.toLocaleTimeString()}.</h2>
+        </div>
+      );
+    }
+  }
+  
+  const root = ReactDOM.createRoot(document.getElementById('app'));
+  root.render(<Clock />);
